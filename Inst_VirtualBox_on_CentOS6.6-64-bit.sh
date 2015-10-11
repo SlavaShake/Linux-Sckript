3021d086516f164b57a2f4b56cb79e270ef142fb
SlavaShake <rdr32@yandex.ru>
2015-10-11 09:38:29 -0700

    Update Inst_VirtualBox_on_CentOS6.6-64-bit.sh

--- Inst_VirtualBox_on_CentOS6.6-64-bit.sh
+++ Inst_VirtualBox_on_CentOS6.6-64-bit.sh
@@ -1,1 +1,6 @@
-yum install binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-PAE-devel dkms
+wget http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
+sudo yum update
+#reboot
+sudo yum install -y binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-PAE-devel dkms
+sudo yum groupinstall -y "Development Tools"
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
